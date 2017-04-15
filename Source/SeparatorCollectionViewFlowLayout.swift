8cd1b57b24ec4d66b023a9bb596210329143c942
Kim de Vos <kimdevos12@hotmail.com>
2017-04-15 11:24:45 +0200

    Update SeparatorCollectionViewFlowLayout.swift
    Updated to Swift3

--- Source/SeparatorCollectionViewFlowLayout.swift
+++ Source/SeparatorCollectionViewFlowLayout.swift
@@ -6,141 +6,127 @@
 //  Copyright © 2015 Chic by Choice. All rights reserved.
 //
 
-import Foundation
-import UIKit
-import UICollectionViewLeftAlignedLayout
-
 public class SeparatorCollectionViewFlowLayout: UICollectionViewLeftAlignedLayout {
 
-  @IBInspectable var separatorWidth: CGFloat = 1 {
-    didSet {
-      invalidateLayout()
-    }
-  }
-
-  @IBInspectable var separatorColor: UIColor = UIColor.blackColor() {
-    didSet {
-      invalidateLayout()
-    }
-  }
-
-  private static let topSeparatorKind = "SeparatorCollectionViewFlowLayout.Top"
-  private static let bottomSeparatorKind = "SeparatorCollectionViewFlowLayout.Bottom"
-  private static let leftSeparatorKind = "SeparatorCollectionViewFlowLayout.Left"
-  private static let rightSeparatorKind = "SeparatorCollectionViewFlowLayout.Right"
-
-  private static let separatorKinds = [leftSeparatorKind, rightSeparatorKind, topSeparatorKind,
-    bottomSeparatorKind]
-
-  init(separatorWidth: CGFloat = 1, separatorColor: UIColor = UIColor.blackColor()) {
-    self.separatorWidth = separatorWidth
-    self.separatorColor = separatorColor
-    super.init()
-  }
-
-  required public init?(coder aDecoder: NSCoder) {
-    super.init(coder: aDecoder)
-  }
-
-  override public class func layoutAttributesClass() -> AnyClass {
-    return ColoredViewLayoutAttributes.self
-  }
-
-  override public func prepareLayout() {
-    super.prepareLayout()
-
-    registerClass(SeparatorView.self,
-      forDecorationViewOfKind: SeparatorCollectionViewFlowLayout.topSeparatorKind)
-    registerClass(SeparatorView.self,
-      forDecorationViewOfKind: SeparatorCollectionViewFlowLayout.bottomSeparatorKind)
-    registerClass(SeparatorView.self,
-      forDecorationViewOfKind: SeparatorCollectionViewFlowLayout.leftSeparatorKind)
-    registerClass(SeparatorView.self,
-      forDecorationViewOfKind: SeparatorCollectionViewFlowLayout.rightSeparatorKind)
-  }
-
-  override public func layoutAttributesForDecorationViewOfKind(elementKind: String,
-    atIndexPath indexPath: NSIndexPath) -> UICollectionViewLayoutAttributes? {
-      super.layoutAttributesForDecorationViewOfKind(elementKind, atIndexPath: indexPath)
-
-      var frame = CGRect.zero
-
-      if let rows = collectionView?.numberOfItemsInSection(0) where indexPath.row < rows {
-        if let cellAttributes = layoutAttributesForItemAtIndexPath(indexPath) {
-          frame = cellAttributes.frame
+    @IBInspectable var separatorWidth: CGFloat = 1 {
+        didSet {
+            invalidateLayout()
         }
-      }
+    }
 
-      let layoutAttributes = ColoredViewLayoutAttributes(forDecorationViewOfKind: elementKind,
-        withIndexPath: indexPath)
+    @IBInspectable var separatorColor: UIColor = UIColor.black {
+        didSet {
+            invalidateLayout()
+        }
+    }
 
-      switch elementKind {
-      case SeparatorCollectionViewFlowLayout.rightSeparatorKind:
-        layoutAttributes.frame = CGRect(x: frame.maxX,
-          y: frame.minY - separatorWidth, width: separatorWidth,
-          height: frame.height + separatorWidth * 2)
-      case SeparatorCollectionViewFlowLayout.leftSeparatorKind:
-        layoutAttributes.frame = CGRect(x: frame.minX - separatorWidth,
-          y: frame.minY - separatorWidth, width: separatorWidth,
-          height: frame.height + separatorWidth * 2)
-      case SeparatorCollectionViewFlowLayout.topSeparatorKind:
-        layoutAttributes.frame = CGRect(x: frame.minX,
-          y: frame.minY - separatorWidth, width: frame.width,
-          height: separatorWidth)
-      case SeparatorCollectionViewFlowLayout.bottomSeparatorKind:
-        layoutAttributes.frame = CGRect(x: frame.minX,
-          y: frame.maxY, width: frame.width,
-          height: separatorWidth)
-      default:
-        break
-      }
+    private static let topSeparatorKind = "SeparatorCollectionViewFlowLayout.Top"
+    private static let bottomSeparatorKind = "SeparatorCollectionViewFlowLayout.Bottom"
+    private static let leftSeparatorKind = "SeparatorCollectionViewFlowLayout.Left"
+    private static let rightSeparatorKind = "SeparatorCollectionViewFlowLayout.Right"
 
-      layoutAttributes.zIndex = -1
-      if let rows = collectionView?.numberOfItemsInSection(0) where indexPath.row >= rows {
-        layoutAttributes.color = UIColor.clearColor()
-      } else {
-        layoutAttributes.color = separatorColor
-      }
+    private static let separatorKinds = [leftSeparatorKind, rightSeparatorKind, topSeparatorKind,
+                                         bottomSeparatorKind]
 
-      return layoutAttributes
-  }
+    init(separatorWidth: CGFloat = 1, separatorColor: UIColor = UIColor.black) {
+        self.separatorWidth = separatorWidth
+        self.separatorColor = separatorColor
+        super.init()
+    }
 
-  override public func layoutAttributesForElementsInRect(rect: CGRect)
-    -> [UICollectionViewLayoutAttributes]? {
-      guard let baseLayoutAttributes = super.layoutAttributesForElementsInRect(rect) else {
-        return nil
-      }
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
 
-      var layoutAttributes = baseLayoutAttributes
-      baseLayoutAttributes.filter { $0.representedElementCategory == .Cell }.forEach {
-        (layoutAttribute) -> () in
+    override public func prepare() {
+        super.prepare()
 
-        layoutAttributes += SeparatorCollectionViewFlowLayout.separatorKinds.flatMap {
-          (kind) -> UICollectionViewLayoutAttributes? in
-            layoutAttributesForDecorationViewOfKind(kind, atIndexPath: layoutAttribute.indexPath)
-          }
-      }
+        register(SeparatorView.self,
+                      forDecorationViewOfKind: SeparatorCollectionViewFlowLayout.topSeparatorKind)
+        register(SeparatorView.self,
+                      forDecorationViewOfKind: SeparatorCollectionViewFlowLayout.bottomSeparatorKind)
+        register(SeparatorView.self,
+                      forDecorationViewOfKind: SeparatorCollectionViewFlowLayout.leftSeparatorKind)
+        register(SeparatorView.self,
+                      forDecorationViewOfKind: SeparatorCollectionViewFlowLayout.rightSeparatorKind)
+    }
 
-      return layoutAttributes
-  }
+    override public func layoutAttributesForDecorationView(ofKind elementKind: String,
+                                                           at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        super.layoutAttributesForDecorationView(ofKind: elementKind, at: indexPath)
 
+        var frame = CGRect.zero
+
+        if let rows = collectionView?.numberOfItems(inSection: 0), indexPath.row < rows {
+            if let cellAttributes = layoutAttributesForItem(at: indexPath) {
+                frame = cellAttributes.frame
+            }
+        }
+
+        let layoutAttributes = ColoredViewLayoutAttributes(forDecorationViewOfKind: elementKind,
+                                                           with: indexPath)
+
+        switch elementKind {
+        case SeparatorCollectionViewFlowLayout.rightSeparatorKind:
+            layoutAttributes.frame = CGRect(x: frame.maxX,
+                                            y: frame.minY - separatorWidth, width: separatorWidth,
+                                            height: frame.height + separatorWidth * 2)
+        case SeparatorCollectionViewFlowLayout.leftSeparatorKind:
+            layoutAttributes.frame = CGRect(x: frame.minX - separatorWidth,
+                                            y: frame.minY - separatorWidth, width: separatorWidth,
+                                            height: frame.height + separatorWidth * 2)
+        case SeparatorCollectionViewFlowLayout.topSeparatorKind:
+            layoutAttributes.frame = CGRect(x: frame.minX,
+                                            y: frame.minY - separatorWidth, width: frame.width,
+                                            height: separatorWidth)
+        case SeparatorCollectionViewFlowLayout.bottomSeparatorKind:
+            layoutAttributes.frame = CGRect(x: frame.minX,
+                                            y: frame.maxY, width: frame.width,
+                                            height: separatorWidth)
+        default:
+            break
+        }
+
+        layoutAttributes.zIndex = -1
+        if let rows = collectionView?.numberOfItems(inSection: 0), indexPath.row >= rows {
+            layoutAttributes.color = UIColor.clear
+        } else {
+            layoutAttributes.color = separatorColor
+        }
+
+        return layoutAttributes
+    }
+
+    override public func layoutAttributesForElements(in rect: CGRect)
+        -> [UICollectionViewLayoutAttributes]? {
+            guard let baseLayoutAttributes = super.layoutAttributesForElements(in: rect) else {
+                return nil
+            }
+
+            var layoutAttributes = baseLayoutAttributes
+            baseLayoutAttributes.filter { $0.representedElementCategory == .cell }.forEach {(layoutAttribute) in
+
+                layoutAttributes += SeparatorCollectionViewFlowLayout.separatorKinds.flatMap {(kind) -> UICollectionViewLayoutAttributes? in
+                    layoutAttributesForDecorationView(ofKind: kind, at: layoutAttribute.indexPath)
+                }
+            }
+
+            return layoutAttributes
+    }
 }
 
 private class ColoredViewLayoutAttributes: UICollectionViewLayoutAttributes {
-
-  var color: UIColor = UIColor.clearColor()
-
+    var color: UIColor = UIColor.clear
 }
 
 private class SeparatorView: UICollectionReusableView {
 
-  override func applyLayoutAttributes(layoutAttributes: UICollectionViewLayoutAttributes) {
-    super.applyLayoutAttributes(layoutAttributes)
+    override func apply(_ layoutAttributes: UICollectionViewLayoutAttributes) {
+        super.apply(layoutAttributes)
 
-    guard let coloredLayoutAttributes = layoutAttributes as? ColoredViewLayoutAttributes else {
-      return
+        guard let coloredLayoutAttributes = layoutAttributes as? ColoredViewLayoutAttributes else {
+            return
+        }
+        backgroundColor = coloredLayoutAttributes.color
     }
-    backgroundColor = coloredLayoutAttributes.color
-  }
-
 }
